c3d9a5382c1b361c5b888579318bbdf4a76685a1
Basster284 <meclersasa370@gmail.com>
2025-04-15 20:47:09 +0500

    lead

--- src/main/java/com/basster/minedustry/items/ModItems.java
+++ src/main/java/com/basster/minedustry/items/ModItems.java
@@ -6,10 +6,13 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.w3c.dom.events.Event;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, main.MODID);
+    //ores
+    public static final RegistryObject<Item> LEAD = ITEMS.register("lead", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
